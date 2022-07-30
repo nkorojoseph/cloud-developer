4a07939e0c0d0421cebfd43f2c76df3d509ac065
Joseph Ahamefula Nkoro <Nkoro@relianceinfosystems.com>
2022-07-30 17:46:50 +0100

    package.json changes

--- course-04/exercises/lesson-4/client/src/components/CreateGroup.tsx
+++ course-04/exercises/lesson-4/client/src/components/CreateGroup.tsx
@@ -47,7 +47,7 @@ export class CreateGroup extends React.PureComponent<
 
       alert('Group was created!')
     } catch (e) {
-      alert('Could not upload an image: ' + e.message)
+      alert('Could not upload an image: ' + e)
     } finally {
       this.setUploadState(false)
     }
